e3fd22bb8843a72e80230efdea9cd3dfe628883f
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-19 13:16:16 +0900

    feat: Add progress #2 'one touch git cloner'

--- one_touch_git_clone/main.py
+++
@@ -1,7 +0,0 @@
-# get arguments of number of unique clones and number of normal clones
-
-# clone
-
-
-# print message
-print("clone successful")
---
+++ one_touch_git_cloner/main.py
@@ -0,0 +1,19 @@
+import sys
+
+args = sys.argv
+
+if len(args) != 2:
+    print ("앱 사용법♥♥: python3 main.py <Clone amount> <Unique clone amount>")
+
+if (type(args[0]) != int) and (type(args[1]) != int):
+    print ("여보♥♥ 우리 넘버만 넣어주세요♥♥: python3 main.py 10 2")
+
+# get arguments of number of unique clones and number of normal clones
+
+
+
+# clone
+
+
+# print message
+print("Clone successful♥♥")
